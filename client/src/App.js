62eeb694830c16e85f41ba4a8cf46f89e3f0e3f9
JulianH <julian.her.arg@gmail.com>
2021-07-05 11:14:44 -0300

    First Commit. Boilerplate creation

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+//aca todavia hay que importar un monton de cosas para ir trabajando
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>VinoTecApp</h1>
+    </div>
+  );
+}
+
+export default App;
