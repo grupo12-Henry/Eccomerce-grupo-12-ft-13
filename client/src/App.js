5d946c73dd976c368bc3165c0c39db58398d1969
Camilo Griffa <camilogriffa@protonmail.com>
2021-07-08 16:57:32 -0300

    update rama login

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,22 @@
 import React from 'react';
 import { Route } from 'react-router';
 import LandingPage from './components/landing/landing';
+import SignUp from './components/sign-up/sign-up';
+// import {Container} from 'react-bootstrap';
+import {AuthProvider} from '../src/contexts/AuthContext';
 
 function App() {
   return (
     <div className="App">
-      <React.Fragment>
-        <Route exact path='/' component={LandingPage}/>
-      </React.Fragment>
+      {/* // TODO: Esto hice yo */}
+      <AuthProvider>
+          {/* <Container className="d-flex align-items-center justify-content-center" style={{minHeight: '100vh'}}> */}
+            {/* <div className="w-100" style={{maxWidth: '400px'}}> */}
+              <Route exact path='/' component={SignUp} />
+            {/* </div> */}
+          {/* <Route exact path='/' component={LandingPage}/> */}
+          {/* </Container> */}
+      </AuthProvider>
     </div>
   );
 }
---
+++ client/src/contexts/AuthContext.js
@@ -0,0 +1,39 @@
+import React, {useContext, useState, useEffect} from 'react';
+import {auth} from '../firebase';
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+};
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState();
+
+  const signup = (email, password) => {
+    return auth.createUserWithEmailAndPassword(email, password);
+  };
+
+  useEffect(()=>{
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setCurrentUser(user);
+    })
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    signup,
+    // logout,
+    // resetPassword,
+    // updateEmail,
+    // updatePassword
+  };
+
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
