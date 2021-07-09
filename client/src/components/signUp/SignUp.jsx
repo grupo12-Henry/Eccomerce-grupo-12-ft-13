415611b99cd769f8e6652909cf89748bac053493
Camilo Griffa <camilogriffa@protonmail.com>
2021-07-09 10:37:38 -0300

    new update newLogin

--- client/src/components/signUp/SignUp.jsx
+++ client/src/components/signUp/SignUp.jsx
@@ -1,7 +1,7 @@
-import React, { useRef, useState } from "react"
-import { Form, Button, Card, Alert } from "react-bootstrap"
-import { useAuth } from "../../contexts/AuthContext"
-import { Link, useHistory } from "react-router-dom"
+import React, { useRef, useState } from "react";
+import { Form, Button, Card, Alert } from "react-bootstrap";
+import { useAuth } from "../../contexts/AuthContext";
+import { Link, useHistory } from "react-router-dom";
 
 export default function Signup() {
   const emailRef = useRef()
