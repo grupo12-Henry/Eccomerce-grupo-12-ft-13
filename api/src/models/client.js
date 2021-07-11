367a1c379ebbbac9467a31ea2e33157593307eae
Emiliano Rodriguez <emilianor81@gmail.com>
2021-07-11 15:10:02 -0300

    acomodar las ramas

--- api/src/models/client.js
+++ api/src/models/client.js
@@ -3,7 +3,7 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
     sequelize.define('client', {
-        /* id: {
+        id: {
             type: DataTypes.INTEGER,
             primaryKey: true,
 
