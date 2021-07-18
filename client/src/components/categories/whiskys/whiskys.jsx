0d70f29a51157433fd26d899a8782000d9b4c59c
rottanico <rottanico@gmail.com>
2021-07-18 19:17:49 -0300

    stock y local storage

--- client/src/components/categories/whiskys/whiskys.jsx
+++ client/src/components/categories/whiskys/whiskys.jsx
@@ -100,7 +100,7 @@ function Whiskys() {
                             <div class="row">
                                 {showProducts &&
                                     showProducts.map(el =>
-                                    (
+                                    {return el.stock>0?<>
                                         <div class="col-md-4 mt-2">
                                             <div class="card">
                                                 <div class="card-body">
@@ -142,7 +142,7 @@ function Whiskys() {
                                                 </div>
                                             </div>
                                         </div>
-                                    ))}
+                                      </>:null})}
                   </div>
                   <div class="justify-content-center">
                     <button
