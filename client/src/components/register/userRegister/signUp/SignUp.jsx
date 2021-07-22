e3982a04572e42335c732733d0736bf916e11ff5
JulianH <julian.her.arg@gmail.com>
2021-07-22 11:18:59 -0300

    changes in signup component. Register fixed

--- client/src/components/register/userRegister/signUp/SignUp.jsx
+++ client/src/components/register/userRegister/signUp/SignUp.jsx
@@ -1,7 +1,5 @@
 import React, { useRef, useState } from "react";
-import { useDispatch, useSelector }from "react-redux";
-import {crearUsuario} from '../../../../actions/index.js';
-
+// import { Form, Button, Card, Alert, Container } from "react-bootstrap";
 import { useAuth } from "../../../../contexts/AuthContext";
 import { Link, useHistory } from "react-router-dom";
 import { StyledDiv } from "./styled";
@@ -9,30 +7,6 @@ import '../signUp/signup.css'
 
 
 export default function Signup({ onClose }) {
-  const [user, setUser] = useState({ 
-   name: '',
-   lastName:'',
-   phone:'',
-   adress:'',
-   state:'',
-   mail: '',  
-   identityCard:'' 
-})
-const handleUser = (e) => {
-  setUser({
-      ...user,
-      [e.target.name]: e.target.value
-  })
-}
-const dispatch = useDispatch();
-
-  const nombreRef = useRef();
-  const apellidoRef = useRef();
-  const nacimientoRef = useRef();
-  const direccionRef = useRef();
-  const provinciaRef = useRef();
-  const codigoPostalRef = useRef();
-  const telefonoRef = useRef();
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
@@ -49,13 +23,9 @@ const dispatch = useDispatch();
     }
 
     try {
-      console.log("Mail:", emailRef.current.value);
-      console.log("Pass:", passwordRef.current.value);
-      console.log("PassConf:", passwordConfirmRef.current.value);
       setError("");
       setLoading(true);
-      dispatch(crearUsuario(user));
-      signup(emailRef.current.value, passwordRef.current.value);
+      await signup(emailRef.current.value, passwordRef.current.value);
       // onClose()
       history.push("/home");
     } catch {
@@ -76,41 +46,9 @@ const dispatch = useDispatch();
                 
                 <p>{error}</p>
                 <form onSubmit={handleSubmit}>
-                <div className='email'>
-                  <label>Nombre:</label>
-                  <input onChange={handleUser} className='input_email'value={user.name} type="text" name="name" ref={nombreRef}></input>
-                  </div>
-                  <div className='email'>
-                  <label>Apellido:</label>
-                  <input onChange={handleUser} className='input_email' type="text" name="lastName" value={user.lastName} ref={apellidoRef}></input>
-                  </div>
-                  <div className='email'>
-                  <label>Fecha de Nacimiento:</label>
-                  <input className='input_email' type="date" name="nacimiento" ref={nacimientoRef}></input>
-                  </div>
-                  <div className='email'>
-                  <label>DNI:</label>
-                  <input onChange={handleUser} className='input_email' type="number" name="identityCard" value={user.identityCard}></input>
-                  </div>
-                  <div className='email'>
-                  <label>Telefono:</label>
-                  <input onChange={handleUser} className='input_email' type="number" name="phone" value={user.phone} ref={telefonoRef}></input>
-                  </div>
-                  <div className='email'>
-                  <label>Direccion:</label>
-                  <input onChange={handleUser} className='input_email' type="text" name="adress" value={user.adress} ref={direccionRef}></input>
-                  </div>
-                  <div className='email'>
-                  <label>Provincia:</label>
-                  <input  onChange={handleUser} className='input_email' type="text" name="state" value={user.state} ref={provinciaRef}></input>
-                  </div>
-                  <div className='email'>
-                  <label>Codigo Postal:</label>
-                  <input className='input_email' type="number" name="codigoPostal" ref={codigoPostalRef}></input>
-                  </div>                  
                   <div className='email'>
                   <label>Correo Electronico:</label>
-                  <input onChange={handleUser}className='input_email' type="text" value={user.mail} name="mail" ref={emailRef}></input>
+                  <input className='input_email' type="text" name="email" ref={emailRef}></input>
                   </div>
                   <br/>
                   <div className='password'>
@@ -128,7 +66,7 @@ const dispatch = useDispatch();
                   ></input>
                   </div>
                   <br/>
-                  <button onClick={(e)=>handleSubmit(e)} disabled={loading} className="btn btn-light" type="submit">
+                  <button disabled={loading} className="btn btn-light" type="submit">
                     Registrate
                   </button>
                 </form>
