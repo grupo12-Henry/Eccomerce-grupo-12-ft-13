c0a460af211e9a814f07cd0bc318fedb2bb8fbc4
JulianH <julian.her.arg@gmail.com>
2021-08-02 18:06:07 -0300

    styles in map and contactoForm

--- client/src/components/dashboard-user/pages/contacto/Contacto.jsx
+++ client/src/components/dashboard-user/pages/contacto/Contacto.jsx
@@ -28,48 +28,44 @@ if (!loading) {
   } else {
   return (
     <>
-    <Maps />
-    <div>
-    <div className="formularioDeContacto">
-      <section>
-        <h3 class="h1-responsive text-left my-4 ml-5 mb-3">Contáctenos</h3>
-        <div>
-          <div>
-            <form onSubmit={sendEmail}>
-              <div class="row">
-                <div class="col-md-6">
-                  <div class=" md-form mb-0 ">
-                    <label class="ml-5 mb-3 form-label">Nombre: </label>
-                    <input class="ml-5 mb-3 form-control" type="text" name="name" />
-                  </div>
-                  <div>
-                    <div class="ml-5 mb-3 col-md-14">
-                      <div class="mmd-form mb-0">
-                        <label for="email" class="form-label" >Correo electronico: </label>
-                        <input class="form-control" type="email" name="user_email" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-md-12">
-                    <div class="ml-5 mb-3 md-form">
-                      <label class="form-label">Mensaje</label>
-                      <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea" />
-                    </div>
-                  </div>
-                </div>
-                <div className="ml-5 mb-3 text-center">
-                  <input className="btn btn-dark mb-2" type="submit" value="Enviar" />
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </section>
+    <div class='container-fluid Responsive' >
+    <div class='pepe'>
+      <Maps />
     </div>
+    <div class='ml-5 mt-4 mr-5'>
+      <div className="formularioDeContacto">
+        <section>
+          <h3 class="h1-responsive text-left my-4 mb-3">Contáctenos</h3>
+            <div>
+              <form onSubmit={sendEmail}>
+                <div class="row">
+                  <div class="col-lg-12">
+                    <div class=" md-form mb-0">
+                      <label class=" mb-3 form-label">Nombre: </label>
+                      <input class=" mb-3 form-control" type="text" name="name" />
+                          <label for="email" class="form-label mb-3" >Correo electronico: </label>
+                          <input class="form-control mb-3" type="email" name="user_email" />
+                    </div>
+                  </div>
+                </div>
+                  <div class="row">
+                    <div class="col-lg-12 mb-3">
+                      {/* <div class="ml-5 mb-3 md-form"> */}
+                        <label class="form-label">Mensaje</label>
+                        <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea" />
+                      {/* </div> */}
+                    </div>
+                  </div>
+                  <div className="mb-3 text-center">
+                    <input className="btn btn-dark mb-2" type="submit" value="Enviar" />
+                </div>
+              </form>
+            </div>
+        </section>
+      </div>
     </div>
     <div className="fotter">
+    </div>
     </div>
     </>
   );
