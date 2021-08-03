6228846d8731269ff36fb5a44dadb1f7a9e4e817
Emiliano Rodriguez <emilianor81@gmail.com>
2021-08-03 13:42:55 -0300

    responsive de vista compras

--- client/src/components/shoppingCart/CartItem.jsx
+++ client/src/components/shoppingCart/CartItem.jsx
@@ -5,7 +5,6 @@ import { removeProductCart, addProductCart, updateProductCart, getProducts } fro
 import './CartItem.css';
 
 function CartItem({ data }) {
-  // const [state, setState]= useState(1)
   const [precioTotal, setPrecioTotal] = useState(data.price * data.cantidad)
   const ProductsCart = useSelector((state) => state.productCart)
   const product = useSelector((state) => state.products);
@@ -17,12 +16,6 @@ function CartItem({ data }) {
 }, []);
 
 
-// cada vez q cambie el cant, se impacte en el productCart
-//crear una action que cambie el valor de la cantidad,
-//traerme aqui el useDispatch y ejecutar esa action en el onChange de el cant ese, y le paso la cantidad
-//para que cambie el product card, y actualice el localstorage
-
-// console.log(data)
 
 useEffect(() => {
   dispatch(getProducts());
@@ -64,18 +57,17 @@ useEffect(() => {
     window.localStorage.setItem("array", JSON.stringify((!!array)?array.filter(e => e.id !== data.id).concat([{...data, cantidad: parseInt(data.cantidad)}]):array=[{...data, cantidad: parseInt(data.cantidad)}]))
     dispatch(updateProductCart({...data, cantidad:data.cantidad>data.stock?data.stock:data.cantidad}))//typeof data.cantidad === Number ?:1
     setPrecioTotal((data.cantidad>data.stock)?data.stock * data.price:data.cantidad*data.price)
-    //VER ESTE PROCEDIMIENTO, AQUI DEBE SETEARSE LA CANTIDAD DEL CARRITO EN PRODUCTCART
   }
-// console.log(typeof data.cantidad)
   return (
     <div >
-        <div className='container-item d-flex row'>
-          <img src={data.image} className='Img col-2' alt='imagen' />
-          <h6 className='col-3' >{data.name}</h6>
-          <h6 className='col-2'>$ {data.price}.00</h6>
-          <input type='number' min={1} max={data.stock} onClick={alertStock} onChange={handleCountChange} value={(data.cantidad>data.stock)? data.stock:data.cantidad} className='counter col-1' name='count' autoComplete='off' />
-          <h6 className='col-2'>$ {precioTotal}.00</h6>
-          <button type="button" className='btn btn-danger col-1' onClick={() => delFromCart()} >✖</button>
+        <div id='contenedor' className='container-item d-flex row'>
+            <img src={data.image} className='Img col-sm-2' alt='imagen' />
+            <h6 id='iName' className='col-sm-3' >{data.name}</h6>
+            <h6 id='iPrice'className='col col-sm-2'>$ {data.price}.00</h6>
+            <input id='iCounter' className='counter col col-sm-1' type='number' min={1} max={data.stock} onClick={alertStock} onChange={handleCountChange} value={(data.cantidad>data.stock)? data.stock:data.cantidad}  name='count' autoComplete='off' />
+            <h6 id='iPriceT'className=' col col-sm-2 '>$ {precioTotal}.00</h6>
+            <button id='bClear' type="button" className='btn btn-danger col-1 col-sm-1 ' onClick={() => delFromCart()} >✖</button>
+        <hr />
         </div>
     </div>
   )
